b00b1119fa7e34cf920d87e01aca6046543aa2ef
mitrang <mitrang.lad@smartsensesolutions.com>
2022-11-16 12:38:28 +0530

    basic express route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi hello there");
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div><h1>Hi from express</h1></div>
+    `)
+})
+
+app.listen(3000, () => {
+    console.log('listeing on port 3000')
+})
